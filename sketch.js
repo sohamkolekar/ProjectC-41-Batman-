eea000d1414946acec923332573546088c2593f9
sohamkolekar <68347705+sohamkolekar@users.noreply.github.com>
2020-09-12 00:03:58 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -4,11 +4,14 @@ const Events=Matter.Events;
 const Bodies=Matter.Bodies;
 const Body=Matter.Body;
 
-var drop=[],man,ground;
+var drop=[],man,ground,lightning;
+var lightning1,lightning2,lightning3;
 var maxDrops=300;
 
 function preload(){
-   
+   lightning1=loadImage("lightning1.png");
+   lightning2=loadImage("lightning2.png");
+   lightning3=loadImage("lightning3.png");
 }
 
 function setup(){
@@ -18,6 +21,8 @@ function setup(){
 
  ground=new Ground(400,490,800,20);
  
+ lightning=createSprite(400,100,5,5);
+ lightning.visible=false;
  //drop=new Drops(200,200);
  man=new Man(400,407);
 
@@ -37,10 +42,30 @@ ground.display();
 for(var i=0; i<drop.length; i++){
     drop[i].display();
 }
-
+spawnLightning();
 console.log(mouseY);
 man.display();
  drawSprites();   
 
 }   
+
+function spawnLightning(){
+ if(frameCount%50===0){
+    lightning.visible=true;
+   var type=Math.round(random(1,3))
+   switch(type){
+    case 1: lightning.addImage(lightning1);
+            break;
+    case 2: lightning.addImage(lightning2);       
+            break
+    case 3: lightning.addImage(lightning3);
+            break;
+    default : break;            
+   }
+ }
+ else{
+     lightning.visible=false;
+ }
+
+}
 
